ec46619a8f4231ae510e0c9f295741c973589bf3
ATDS <atechdesignsolutions@gmail.com>
2022-04-30 11:59:04 +1200

    updated errors

--- app.py
+++ app.py
@@ -20,19 +20,16 @@ app.config['SECRET_KEY'] = 'secret'
 mail = Mail(app)
 
 
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'), 500
-    
-    
-
-@app.route('/500')
-def error500():
-    abort(500)
-
+#404 route
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(e):
     return render_template('404.html'), 404
+
+
+#500 route
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
